20f92685c4068a61a44d27181c5d428132766c10
PRIYANSHDUBEY <priyanshdubey787@gmail.com>
2024-01-17 15:36:56 +0530

    first done

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
-
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [text, setText] = useState("");
+
+  const handleInput = (e) => {
+    setText(e.target.value);
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <p>Enter your name:</p>
+      <form action="">
+        <input type="text" value={text} onChange={handleInput} />
+      </form>
+      
+      {text && <p>Hello {text}</p>}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
